68bba5ba0cf53624a8ec8929a4d8209b722577cb
Thiago <thiago8jesusaraujo@gmail.com>
2024-12-19 12:09:41 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,8 +6,9 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
-	Color color;
-
+	private Color color;
+	private int moveCount;
+	
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -19,6 +20,18 @@ public abstract class ChessPiece extends Piece {
 
 	public Color getColor() {
 		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	
+	public void decreaseMoveCount() {
+		moveCount--;
 	}
 	
 	protected boolean isThereOpponentPiece(Position position) {
